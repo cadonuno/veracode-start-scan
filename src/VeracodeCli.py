05ceee3ba92ae7c1c80bf3f016fe1c898e51ec03
Ricardo Pereira - Veracode <20283870+cadonuno@users.noreply.github.com>
2025-01-06 10:58:46 +0000

    Adding option to ignore auto package artifact

--- src/VeracodeCli.py
+++ src/VeracodeCli.py
@@ -5,7 +5,12 @@ from ScanConfiguration import ScanConfiguration
 
 def package_application(scan_source: str, scan_configuration: ScanConfiguration) -> str:
     call_subprocess("Veracode Packager", scan_configuration, True, [scan_configuration.veracode_cli_location, "package", "-das", scan_source, "--output", PACKAGER_OUTPUT])
-    return os.path.join(scan_configuration.base_cli_directory, PACKAGER_OUTPUT)
+    artifacts_directory = os.path.join(scan_configuration.base_cli_directory, PACKAGER_OUTPUT)
+    for ignore_artifact in scan_configuration.ignore_artifacts:
+        ignore_path = os.path.join(artifacts_directory, ignore_artifact)
+        if os.path.exists(ignore_path):
+            os.remove(ignore_path)
+    return artifacts_directory
 
 def get_policy_file_name(scan_configuration: ScanConfiguration) -> str:
     policy_name = scan_configuration.policy_name.replace("+","%2B")
